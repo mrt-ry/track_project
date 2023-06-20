6d30fa8c3991b2812c652189c9123b93e7a14de5
ryouya murata <ryouya.murata@i-standard.jp>
2023-06-20 12:40:36 +0900

    Beanを登録

--- src/main/java/com/example/demo/web/milestone/MilestoneController.java
+++ src/main/java/com/example/demo/web/milestone/MilestoneController.java
@@ -1,28 +1,24 @@
 package com.example.demo.web.milestone;
 
-import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import com.example.demo.domain.milestone.MilestoneEntity;
+import com.example.demo.domain.milestone.MilestoneService;
+
+import lombok.AllArgsConstructor;
 
 @Controller
+@AllArgsConstructor
 public class MilestoneController {
+
+    private final MilestoneService milestoneService = new MilestoneService();
 
     @GetMapping("/milestones")
     public String showList(Model model) {
-
-        var milestoneList = List.of(
-                new MilestoneEntity(1, "1", "1"),
-                new MilestoneEntity(2, "2", "2"),
-                new MilestoneEntity(3, "3", "3"));
-
-        model.addAttribute("milestoneList", milestoneList);
-
+        model.addAttribute("milestoneList", milestoneService.findAll());
         return "milestones/list";
-
     }
 
 }
