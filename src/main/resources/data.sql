d19860a23eccb4b4f8bb0fe7dc904658d1b448e9
Ryoya Murata <135285025+mrt-ry@users.noreply.github.com>
2023-06-29 14:05:34 +0900

    Delete src directory

--- src/main/resources/data.sql
+++
@@ -1,6 +0,0 @@
-insert into milestones (milestone, status,priority,date,category) values ('2023年1月1日:ローンチ', '未完了',3,'2000-12-21','プログラミング');
-insert into milestones (milestone, status,priority,date,category) values ('2023年2月1日:訪問', '完了',3,'2000-12-25','営業');
-insert into milestones (milestone, status,priority,date,category) values ('2023年1月1日:開発', '未着手',3,'2001-12-28','Java');
-
-insert into users (userName,password) values ('iwashi','$2a$10$T7dckfpU4EC0nKxEKKYukeTa1ZXGBtKc5ZrEFfVNZ/CGoYmUDRE3e');
-insert into users (userName,password) values ('murata','b');
